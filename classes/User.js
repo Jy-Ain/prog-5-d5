3cafaa04aee35f0fca7a5f7e41dde4732ac30318
Jy-Ain <servicesjnankorondrano@gmail.com>
2025-05-28 09:16:24 +0300

    feat: implement all actions of User

---
+++ classes/User.js
@@ -0,0 +1,18 @@
+import readlineSync from 'readline-sync';
+
+export class User {
+    selectDrink() {
+        const drink = readlineSync.question("Choisissez une boisson (cafe / the / chocolat) : ");
+        return drink?.toLowerCase();
+    }
+
+    insertPayment() {
+        const input = readlineSync.question("Insérez le montant (en Ar) : ");
+        const amount = parseFloat(input);
+        return isNaN(amount) ? 0 : amount;
+    }
+
+    collectDrink() {
+        console.log("Vous avez récupéré votre boisson.");
+    }
+}
